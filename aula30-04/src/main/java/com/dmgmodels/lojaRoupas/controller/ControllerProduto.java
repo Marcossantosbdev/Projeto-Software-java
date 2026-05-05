f31b35b11364c87b56a4d9337f19bad7520c2aef
Marcos V. Bueno <marcosssantosbuenocontato@gmail.com>
2026-05-05 09:09:41 -0300

    asd

---
+++ aula30-04/src/main/java/com/dmgmodels/lojaRoupas/controller/ControllerProduto.java
@@ -0,0 +1,17 @@
+package com.dmgmodels.lojaRoupas.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/roupa")
+
+public class ControllerProduto {
+    @PostMapping
+    public void cadastrarProduto (@RequestBody String produto){
+        System.out.println(produto);
+    }
+}
