d986d67b1df4c10704dfd059f3daaffe19e6400f
Marcos V. Bueno <marcosssantosbuenocontato@gmail.com>
2026-05-07 10:31:26 -0300

    Work in Progress

--- aula30-04/src/main/java/com/dmgmodels/lojaRoupas/controller/ControllerProduto.java
+++ aula30-04/src/main/java/com/dmgmodels/lojaRoupas/controller/ControllerProduto.java
@@ -1,17 +1,28 @@
 package com.dmgmodels.lojaRoupas.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dmgmodels.lojaRoupas.model.DadosCadastroRoupa;
+import com.dmgmodels.lojaRoupas.model.Roupa;
+import com.dmgmodels.lojaRoupas.model.Tamanho;
+import com.dmgmodels.lojaRoupas.repository.RoupaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/roupa")
 
 public class  ControllerProduto {
+
+    @Autowired
+    private RoupaRepository roupaRepository;
+
     @PostMapping
-    public void cadastrarProduto (@RequestBody String produto){
-        System.out.println(produto);
+    public void cadastrarProduto(@RequestBody DadosCadastroRoupa dados){
+        roupaRepository.save(new Roupa(dados));
+    }
+
+    @GetMapping
+    public List<Roupa> listarProdutos(){
+        return roupaRepository.findAll();
     }
 }
